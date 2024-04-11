b3d972669f2807069255a07dd1a5d9b71d14e824
William Luo <william2299@gmail.com>
2024-04-11 05:04:20 -0600

    Refactor Portfolio component layout

--- minimal-portfolio/src/components/Portfolio.jsx
+++ minimal-portfolio/src/components/Portfolio.jsx
@@ -4,7 +4,7 @@ import PortfolioItem from './PortfolioItem';
 
 function Portfolio() {
    return (
-      <div className="flex flex-col md:flex-row items-center justify-center">
+      <div className="flex flex-col items-center justify-center md:flex-row ">
          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {portfolio.map(project => (
                <PortfolioItem 
--- minimal-portfolio/src/data/portfolio.js
+++ minimal-portfolio/src/data/portfolio.js
@@ -1,7 +1,7 @@
 // Add projects here
  export default [
     {
-        title: 'The Conversationalist',
+        title: "Hackathon 2024 'The Conversationalist'",
         imgUrl: '/assets/images/theconversationalist.PNG',
         stack: ['React', 'Next.js', 'TypeScript','Google Cloud','OpenAI API', 'Docker'],
         link: 'https://reformed-22an6t4ebq-uc.a.run.app/'
@@ -14,8 +14,32 @@
     },
     {
         title: 'Cancer Prediction using Machine Learning',
-        imgUrl: '/assets/images/cancerprediction.PNG',
+        imgUrl: '/assets/images/cancerinsights.PNG',
         stack: ['Python', 'Pandas', 'NumPy', 'Machine Learning'],
+        link: 'https://cancerinsights.streamlit.app/'
+    },
+    {
+        title: 'NHL postivity index',
+        imgUrl: '/assets/images/nhlpositivityindex.PNG',
+        stack: ['Python', 'Pandas', 'NumPy', 'Machine Learning'],
+        link: 'https://uais.dev/projects/nhl-positivity-index/'
+    },
+    {
+        title: 'Morgan Stanley Hackathon 2024',
+        imgUrl: '/assets/images/Rarequest.PNG',
+        stack: ['HTML', 'CSS', 'JavaScript', 'Phaser'],
+        link: 'https://rqmo12-game.web.app/'
+    },
+    {
+        title: 'Superchat Firebase App',
+        imgUrl: '/assets/images/superchat.PNG',
+        stack: ['HTML', 'CSS', 'JavaScript', 'Firebase'],
         link: ''
-    }
+    },
+    {
+        title: 'Antrea',
+        imgUrl: '/assets/images/antrea.PNG',
+        stack: ['Go', 'Shell', 'Kubernetes'],
+        link: 'https://antrea.io/'
+    },
  ]
